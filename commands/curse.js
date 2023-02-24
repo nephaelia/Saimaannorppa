6efe769770d18af280b75d021792099c0cf0cfc2
Nephaelia <105744885+nephaelia@users.noreply.github.com>
2023-02-23 21:58:14 -0500

    Reverting back to duplicate arrays...

--- commands/curse.js
+++ commands/curse.js
@@ -17,6 +17,15 @@ module.exports = {
             'Fuck this shit!',-
             'God damn it',
             'Fuck you',
+            'Fuck off!',
+			'Stupid Dummy McDumb-Face!',
+			'Oh poop!',
+			'Holy shit!',
+			'Buttface',
+			'Cunt!',
+			'Hey fuckface!',
+			'O shit waddup',
+			'Damn you all to Hell!',
            ];
         const response = curses[Math.floor(Math.random() * curses.length)];
         await interaction.reply(response);
--- index.js
+++ index.js
@@ -22,8 +22,29 @@ client.on(Events.InteractionCreate, async interaction => {
 	} else if (commandName === 'p') {
 		await interaction.reply('Perkele!');
 	} else if (commandName ==='curse') {
-		const curses = [CURSES];
-		   const response = curses[Math.floor(Math.random() * curses.length)];
+		const curses = [
+			'Fuck!',
+			'You son of a bitch',
+			'Bastard!',
+			'Perkele!',
+			'My arse!',
+			'Asshole!',
+			'Motherfucker!',
+			'Schieße!',
+			'Fuck this shit!',
+			'God damn it',
+			'Fuck you',
+			'Fuck off!',
+			'Stupid Dummy McDumb-Face!',
+			'Oh poop!',
+			'Holy shit!',
+			'Buttface',
+			'Cunt!',
+			'Hey fuckface!',
+			'O shit waddup',
+			'Damn you all to Hell!',
+		   ];
+		const response = curses[Math.floor(Math.random() * curses.length)];
 		await interaction.reply(response);
 	   }
 });
