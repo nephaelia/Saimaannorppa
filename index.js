1501ce05843848ced73ac838ce0564465c1a57c7
Nephaelia <105744885+nephaelia@users.noreply.github.com>
2023-03-09 12:32:06 -0500

    starting to update command handling following discord.js tutorial part I missed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { Client, Events, GatewayIntentBits, Partials, GuildMember, Role, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collector } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, Partials, GuildMember, Role, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collector } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ 
+// Need to check which intents I really need/only add the ones I really need
 	intents: [
 		GatewayIntentBits.Guilds, 
 		GatewayIntentBits.GuildEmojisAndStickers, 
@@ -18,6 +21,24 @@ const client = new Client({
 		Partials.User
 	]
 	 });
+	 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
